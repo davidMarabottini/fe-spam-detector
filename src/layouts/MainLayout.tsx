bb0253e6e35f9347934994c1f9178b7773c65855
davidMarabottini <david.marabottini@gmail.com>
2026-01-14 12:30:57 +0100

    feat: improved logic of authentication, header and menu, divided usermenu from header

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,7 +1,5 @@
 import Header from "@components/organisms/Header/Header";
-import { useAuth } from "../auth/useAuth";
-import { Outlet, useMatches } from 'react-router-dom';
-import { useLogout } from "@/hooks/api/useAuthenticationHooks";
+import { Outlet, useMatches, type UIMatch } from 'react-router-dom';
 import Typography from "@/components/atoms/Typography/Typography";
 import styles from './MainLayout.module.scss';
 import { useTranslation } from "react-i18next";
@@ -9,28 +7,33 @@ import Card from "@/components/atoms/Card/Card";
 import { ToastContainer } from "@/components/organisms/Toast/ToastContainer";
 import clsx from "clsx";
 import { useMemo } from "react";
+import type { RouteHandle } from "@/constants/routes";
 
 export const MainLayout = () => {
-  const matches = useMatches();
-  const {mutate: logout} = useLogout();
+  const matches = useMatches() as Array<UIMatch & { handle?: RouteHandle }>;
   const {t} = useTranslation("menu");
-
-  const { user } = useAuth();
   
   const label = useMemo(() => {
-  return matches?.at?.(-1)?.handle?.label || '';
-}, [matches]);
+    return matches?.at?.(-1)?.handle?.label || '';
+  }, [matches]);
+
+  const mainClass = clsx(styles['cl-layout__main'], "l-container l-content-section")
 
   return (
     <div className="l-main-layout">
-      <Header logout={user ? logout : undefined} userDetails={user || null}/>
-      <main className={clsx(styles['cl-layout__main'], "l-container l-content-section")}>
+      <Header />
+
+      <main className={mainClass}>
         <Card additionalClassName={styles['cl-layout__card-title']}>
-          <Typography as="h2" variant="h2" additionalClasses={styles['cl-layout__title']}>
+          <Typography
+            as="h2"
+            variant="h2"
+            additionalClasses={styles['cl-layout__title']}
+          >
            {t(label)}
           </Typography>
         </Card>
-        <ToastContainer  />
+        <ToastContainer />
         <Outlet />
       </main>
     </div>
